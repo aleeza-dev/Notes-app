93922cd3c4bc7eb93a5ee848bb44f16213d9203b
Aleeza Amjad <alizaamjad597@gmail.com>
2026-08-03 13:34:41 +0500

    Connect frontend with Railway backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://notes-app-alpha-tawny.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 
