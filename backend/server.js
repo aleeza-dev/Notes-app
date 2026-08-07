1c3779071b26e44281fdc35a6f60fa9711529767
Aleeza Amjad <alizaamjad597@gmail.com>
2026-08-07 14:30:53 +0500

    Update CORS for new Vercel URL

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://notes-app-gxt2-pr1wmtdxc-aleeza-amjads-projects.vercel.app",
+      "https://notes-exsbepz2s-aleeza-amjads-projects.vercel.app",
       "https://notes-app-iota-eight-43.vercel.app"
     ],
     credentials: true,
